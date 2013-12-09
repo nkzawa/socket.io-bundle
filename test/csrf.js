6637313974c788724641d4c384cd64643769707e
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2013-12-10 03:55:11 +0900

    update connect module and fix csrf middleware

--- test/csrf.js
+++ test/csrf.js
@@ -6,8 +6,6 @@ var expect = require('chai').expect
   , client = support.client;
 
 
-var sessionCookie = support.sessionCookie;
-
 describe('csrf', function() {
   beforeEach(support.startServer);
   afterEach(support.stopServer);
@@ -20,7 +18,7 @@ describe('csrf', function() {
     app.use(connect.session({secret: 'greg', store: store}));
     app.use(connect.csrf());
     app.use(function(req, res) {
-      res.end(req.session._csrf || 'none');
+      res.end(req.csrfToken() || 'none');
     });
 
     var io = this.io;
@@ -50,7 +48,7 @@ describe('csrf', function() {
     app.use(connect.session({secret: 'greg', store: store}));
     app.use(connect.csrf());
     app.use(function(req, res) {
-      res.end(req.session._csrf || 'none');
+      res.end(req.csrfToken() || 'none');
     });
 
     var io = this.io;
@@ -81,7 +79,7 @@ describe('csrf', function() {
     app.use(connect.session({secret: 'greg', store: store}));
     app.use(connect.csrf());
     app.use(function(req, res) {
-      res.end(req.session._csrf || 'none');
+      res.end(req.csrfToken() || 'none');
     });
 
     var io = this.io;
@@ -107,7 +105,7 @@ describe('csrf', function() {
     app.use(connect.session({secret: 'greg', store: store}));
     app.use(connect.csrf());
     app.use(function(req, res) {
-      res.end(req.session._csrf || 'none');
+      res.end(req.csrfToken() || 'none');
     });
 
     var io = this.io;
@@ -121,6 +119,47 @@ describe('csrf', function() {
       socket.once('error', function(err) {
         expect(err).to.eql('Forbidden');
         done();
+      });
+    }).end();
+  });
+
+  it('should keep the original method value', function(done) {
+    var store = new bundle.session.MemoryStore();
+
+    var app = this.app;
+    app.use(connect.cookieParser());
+    app.use(connect.session({secret: 'greg', store: store}));
+    app.use(connect.csrf());
+    app.use(function(req, res) {
+      res.end(req.csrfToken() || 'none');
+    });
+
+    var io = this.io
+      , method;
+
+    io.use(bundle.cookieParser())
+    io.use(bundle.session({secret: 'greg', store: store}))
+    io.use(function(socket, next) {
+      method = socket.request.method;
+      next();
+    })
+    io.use(bundle.csrf())
+    io.use(function(socket, next) {
+      expect(socket.request.method).to.eql(method);
+      done();
+    })
+
+    http.request('http://localhost:8888', function(res) {
+      var cookie = res.headers['set-cookie'][0]
+        , body = '';
+
+      res.on('data', function(chunk) {
+        body += chunk;
+      });
+      res.on('end', function() {
+        var socket = client('/?_csrf=' + encodeURIComponent(body),
+                {headers: {Cookie: cookie}});
+        socket.on('connect', done);
       });
     }).end();
   });
