9d7d49ec39ae719bea72f7b3cf7145b919a0beb1
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2013-12-11 02:44:31 +0900

    refactor tests for csrf middleware

--- test/csrf.js
+++ test/csrf.js
@@ -7,8 +7,38 @@ var expect = require('chai').expect
 
 
 describe('csrf', function() {
+  var io, cookie, csrfToken;
+
   beforeEach(function(done) {
-    support.startServer(this, done);
+    var self = this;
+
+    support.startServer(this, function() {
+      var store = new bundle.session.MemoryStore();
+
+      var app = self.app;
+      app.use(connect.cookieParser());
+      app.use(connect.session({secret: 'greg', store: store}));
+      app.use(connect.csrf());
+      app.use(function(req, res) {
+        res.end(req.csrfToken() || 'none');
+      });
+
+      io = self.io;
+      io.use(bundle.cookieParser())
+      io.use(bundle.session({secret: 'greg', store: store}))
+      io.use(bundle.csrf())
+
+      http.request('http://localhost:8888', function(res) {
+        cookie = res.headers['set-cookie'][0];
+        csrfToken = '';
+
+        res.on('data', function(chunk) {
+          csrfToken += chunk;
+        });
+
+        res.on('end', done);
+      }).end();
+    });
   });
 
   afterEach(function(done) {
@@ -16,157 +46,40 @@ describe('csrf', function() {
   });
 
   it('should work with a valid token', function(done) {
-    var store = new bundle.session.MemoryStore();
-
-    var app = this.app;
-    app.use(connect.cookieParser());
-    app.use(connect.session({secret: 'greg', store: store}));
-    app.use(connect.csrf());
-    app.use(function(req, res) {
-      res.end(req.csrfToken() || 'none');
-    });
-
-    var io = this.io;
-    io.use(bundle.cookieParser())
-    io.use(bundle.session({secret: 'greg', store: store}))
-    io.use(bundle.csrf())
-
-    http.request('http://localhost:8888', function(res) {
-      var cookie = res.headers['set-cookie'][0]
-        , body = '';
-
-      res.on('data', function(chunk) {
-        body += chunk;
-      });
-      res.on('end', function() {
-        var socket = client('/', {headers: {Cookie: cookie, 'X-CSRF-Token': body}});
-        socket.on('connect', done);
-      });
-    }).end();
+    var socket = client('/', {headers: {Cookie: cookie, 'X-CSRF-Token': csrfToken}});
+    socket.on('connect', done);
   });
 
   it('should work with a valid token via query', function(done) {
-    var store = new bundle.session.MemoryStore();
-
-    var app = this.app;
-    app.use(connect.cookieParser());
-    app.use(connect.session({secret: 'greg', store: store}));
-    app.use(connect.csrf());
-    app.use(function(req, res) {
-      res.end(req.csrfToken() || 'none');
-    });
-
-    var io = this.io;
-    io.use(bundle.cookieParser())
-    io.use(bundle.session({secret: 'greg', store: store}))
-    io.use(bundle.csrf())
-
-    http.request('http://localhost:8888', function(res) {
-      var cookie = res.headers['set-cookie'][0]
-        , body = '';
-
-      res.on('data', function(chunk) {
-        body += chunk;
-      });
-      res.on('end', function() {
-        var socket = client('/?_csrf=' + encodeURIComponent(body),
-                {headers: {Cookie: cookie}});
-        socket.on('connect', done);
-      });
-    }).end();
+    var socket = client('/?_csrf=' + encodeURIComponent(csrfToken),
+            {headers: {Cookie: cookie}});
+    socket.on('connect', done);
   });
 
   it('should fail with an invalid token', function(done) {
-    var store = new bundle.session.MemoryStore();
-
-    var app = this.app;
-    app.use(connect.cookieParser());
-    app.use(connect.session({secret: 'greg', store: store}));
-    app.use(connect.csrf());
-    app.use(function(req, res) {
-      res.end(req.csrfToken() || 'none');
+    var socket = client('/?_csrf=42', {headers: {Cookie: cookie}});
+    socket.once('error', function(err) {
+      expect(err).to.eql('Forbidden');
+      done();
     });
-
-    var io = this.io;
-    io.use(bundle.cookieParser())
-    io.use(bundle.session({secret: 'greg', store: store}))
-    io.use(bundle.csrf())
-
-    http.request('http://localhost:8888', function(res) {
-      var cookie = res.headers['set-cookie'][0];
-      var socket = client('/?_csrf=42', {headers: {Cookie: cookie}});
-      socket.once('error', function(err) {
-        expect(err).to.eql('Forbidden');
-        done();
-      });
-    }).end();
   });
 
   it('should fail with no token', function(done){
-    var store = new bundle.session.MemoryStore();
-
-    var app = this.app;
-    app.use(connect.cookieParser());
-    app.use(connect.session({secret: 'greg', store: store}));
-    app.use(connect.csrf());
-    app.use(function(req, res) {
-      res.end(req.csrfToken() || 'none');
+    var socket = client('/', {headers: {Cookie: cookie}});
+    socket.once('error', function(err) {
+      expect(err).to.eql('Forbidden');
+      done();
     });
-
-    var io = this.io;
-    io.use(bundle.cookieParser())
-    io.use(bundle.session({secret: 'greg', store: store}))
-    io.use(bundle.csrf())
-
-    http.request('http://localhost:8888', function(res) {
-      var cookie = res.headers['set-cookie'][0];
-      var socket = client('/', {headers: {Cookie: cookie}});
-      socket.once('error', function(err) {
-        expect(err).to.eql('Forbidden');
-        done();
-      });
-    }).end();
   });
 
   it('should keep the original method value', function(done) {
-    var store = new bundle.session.MemoryStore();
-
-    var app = this.app;
-    app.use(connect.cookieParser());
-    app.use(connect.session({secret: 'greg', store: store}));
-    app.use(connect.csrf());
-    app.use(function(req, res) {
-      res.end(req.csrfToken() || 'none');
+    io.on('connect', function(socket) {
+      expect(socket.request.method).to.eql('GET');
+      done();
     });
 
-    var io = this.io
-      , method;
-
-    io.use(bundle.cookieParser())
-    io.use(bundle.session({secret: 'greg', store: store}))
-    io.use(function(socket, next) {
-      method = socket.request.method;
-      next();
-    })
-    io.use(bundle.csrf())
-    io.use(function(socket, next) {
-      expect(socket.request.method).to.eql(method);
-      done();
-    })
-
-    http.request('http://localhost:8888', function(res) {
-      var cookie = res.headers['set-cookie'][0]
-        , body = '';
-
-      res.on('data', function(chunk) {
-        body += chunk;
-      });
-      res.on('end', function() {
-        var socket = client('/?_csrf=' + encodeURIComponent(body),
-                {headers: {Cookie: cookie}});
-        socket.on('connect', done);
-      });
-    }).end();
+    client('/?_csrf=' + encodeURIComponent(csrfToken),
+            {headers: {Cookie: cookie}});
   });
 });
 
